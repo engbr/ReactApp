2502a93bad33124c49df3a99eb97b1fe9686e610
sunny <1743761249@qq.com>
2023-12-16 08:24:45 +0800

    new

--- src -基础/03类组件.js
+++ src -基础/03类组件.js
@@ -13,7 +13,7 @@ class App extends Component{
     //reader是每一个类组件都必须要实现的一个钩子函数
       //reader内部是可以写业务逻辑的最终返回的是一个组件模版jsx代码
     render(  ){
-        const a=18.2
+        const a=18.1
         return (<>
         <h1>欢迎React{a}</h1>
         </>)
